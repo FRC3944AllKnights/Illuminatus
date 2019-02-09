37e58b1218200fae2b2892006fb4947c22cad201
Robin-Parmentier <Robin-Parmentier@users.noreply.github.com>
2019-02-09 11:17:17 -0700

    Improved the vision system

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -5,56 +5,65 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-#include <IterativeRobot.h>
-#include <Joystick.h>
+#include <thread> 
+
+#include <cameraserver/CameraServer.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <wpi/raw_ostream.h>
+#include <frc/TimedRobot.h>
+#include <frc/Joystick.h>
+
 #include "Drive.h"
-#include "NotServo.h"
-#include "Pusher.h"
-#include "Lifter.h"
 
 /** This is a test
  * This is a demo program showing how to use Mecanum control with the
  * MecanumDrive class.
  */
-class Robot : public frc::IterativeRobot {
+class Robot : public frc::TimedRobot {
  public:
   void RobotInit() override {
     // Invert the left side motors. You may need to change or remove this to
     // match your robot.
-    COWDrive.init();
-    Servo.originPos();
+    std::thread visionThread(VisionThread);
+    visionThread.detach();
+    LOCDrive.init();
   }
 
   void TeleopPeriodic() override {
     /* Use the joystick X axis for lateral movement, Y axis for forward
      * movement, and Z axis for rotation.
      */
-    COWDrive.mechanum(m_stick.GetX(), m_stick.GetY(), m_stick.GetZ(), m_stick.GetThrottle());
-    Servo.servoRotate(m_stick.GetTrigger());
-    if (m_stick.GetRawButton(6)){
-      lifter.manualUp(m_stick.GetRawButton(6));
-    }
-    else{
-      lifter.manualDown(m_stick.GetRawButton(4));
-    }
-//lifter manual control
-    if (m_stick.GetRawButton(5)){
-      PushMotor.manualPush(m_stick.GetRawButton(5));
-    }
-    else{
-      PushMotor.manualRetract(m_stick.GetRawButton(3));
-    }
-
+    LOCDrive.mechanum(m_stick.GetX(), m_stick.GetY(), m_stick.GetZ(), m_stick.GetThrottle());
   }
 
  private:
+  static void VisionThread() {
+    // Get the USB camera from CameraServer
+    cs::UsbCamera camera =
+        frc::CameraServer::GetInstance()->StartAutomaticCapture("0", 0);
+    // Set the resolution
+    camera.SetResolution(240, 144);
+    // Get a CvSink. This will capture Mats from the Camera
+    cs::CvSink cvSink = frc::CameraServer::GetInstance()->GetVideo();
+    cs::CvSource outputStream =
+        frc::CameraServer::GetInstance()->PutVideo("gray", 240, 144);
+    // Mats are very memory expensive. Lets reuse this Mat.
+    cv::Mat mat;
+
+    while(true) {
+            cvSink.GrabFrame(mat);
+            cvtColor(mat, mat, cv::COLOR_BGR2GRAY);
+            outputStream.PutFrame(mat);
+        }
+  }
  //this is where to change variables
   static constexpr int kJoystickChannel = 0;
-  Drive COWDrive;
-  Lifter lifter;
-  NotServo Servo; //names the class notServo - servo, also creates an instance of servo
-  Pusher PushMotor;
+  Drive LOCDrive;
   frc::Joystick m_stick{kJoystickChannel};
 };
 
-START_ROBOT_CLASS(Robot)
+#ifndef RUNNING_FRC_TESTS
+int main() { return frc::StartRobot<Robot>(); }
+#endif
